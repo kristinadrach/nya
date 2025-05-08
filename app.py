49bd973ca3f40aef7ed7a48d7fc11ad8d7e378cf
kristinadrach <kristinadrach777777@gmail.com>
2025-05-09 00:11:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
+# Считываем index.html как обычный текст
 @app.route("/")
-def home():
-    return render_template("index.html")  # Теперь Flask будет искать index.html в папке templates
+def index():
+    with open("index.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return render_template_string(html)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
